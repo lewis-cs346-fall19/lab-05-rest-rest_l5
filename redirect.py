09639aac3bf222fb986aa902eb94fdb928bc6898
EC2 Default User <ec2-user@ip-172-31-84-85.ec2.internal>
2019-11-01 23:59:41 +0000

    add lab files

---
+++ redirect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import cgi
+import cgitb
+
+print("Content-type: text/html")
+print("Status: 200 OK")
+
+print("""
+<html>
+<head><title> redirect page </title></head>
+<body>
+<h1> Redirect page </h1>
+</body>
+</hmtl>
+""")
---
+++ rest.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+
+import re
+import cgi
+import cgitb
+import os
+import simplejson as json
+import MySQLdb
+import passwords
+
+#cgitb.enable()
+
+conn = MySQLdb.connect(host   = passwords.SQL_HOST,
+                       user   = passwords.SQL_USER,
+                       passwd = passwords.SQL_PASSWD,
+                       db     = "restlab")
+
+url = "ec2-3-83-252-90.compute-1.amazonaws.com/cgi-bin/rest.py/ff/"
+pattern = re.compile("/ff/\d+")
+
+form = cgi.FieldStorage()
+
+if 'PATH_INFO' in os.environ:
+    path_info = os.environ['PATH_INFO']
+else:
+    path_info = ""
+
+
+def index():
+    print("Content-type: text/html")
+    print("Status: 200 OK")
+    print("""
+    <html>
+    <body>
+    <b><font size=+3> Fantasy Football (REST) Website </font></b>
+    <p>
+    <a href="rest.py/ff">Players</a> (GET)
+    <br>
+    <a href="rest.py/new_player">Add Player</a> (POST to Players)
+    <hr>
+    </body></html>
+    """)
+
+
+def newPlayer():
+    print("Content-type: text/html")
+    print("Status: 200 OK")
+    print("""
+    <html>
+    <body>
+    <form action = "add_player" method = POST>
+    <p> Name: <br><input type="text" name="name" maxlength="30"> <br>
+    <p> Position: <br><input type="text" name="pos" maxlength="2"> <br>
+    <p> Week: <br><input type="number" name="week" min="0" max="17"> <br>
+    <p> Points: <br><input type="number" name="pts" min="-50" max = "200" step="0.1"> <br><br>
+    <input type="submit" value="Submit">
+    </form>
+    </body></html>
+    """)
+
+
+def addPlayer():
+    main = "http://ec2-3-83-252-90.compute-1.amazonaws.com/cgi-bin/rest.py"
+
+    name = form.getvalue("name")
+    pos  = form.getvalue("pos")
+    week = form.getvalue("week")
+    pts  = form.getvalue("pts")
+
+    if (name is None or pos is None or week is None or pts is None):
+        print("Content-type: text/html")
+        print("Status: 200 OK")
+        print("""
+        <html>
+        <body>
+        <p>Error: <a href="%s"> Return to main page</a> </p>
+        </body>
+        </html>
+        """ % main)
+    else:
+        query = ("insert into fantasyplayers (name, position, week, points) \
+                values (%s, %s, %s, %s)" % (name, pos, week, pts))
+        cursor = conn.cursor()
+        cursor.execute(query)
+        new_id = cursor.lastrowid
+        cursor.commit()
+        cursor.close()
+        print("Status: 302 Redirect")
+        print("Location: rest.py/ff/%s\n" % new_id)
+
+
+def gets():
+    cursor = conn.cursor()
+    cursor.execute("select * from fantasyplayers;")
+    results = [dict((cursor.description[i][0], value) \
+               for i, value in enumerate(row)) for row in cursor.fetchall()]
+    cursor.close()
+    for item in results:
+        item["url"] = url + str(item["id"])
+
+    json_id = path_info.strip('/').split('/')[-1]
+
+    if json_id.isdigit():
+        print("Content-type: application/json")
+        print("Status: 200 OK\n")
+        print(json.dumps(results[int(json_id)]))
+    else:
+        results_json = json.dumps(results, indent=2)
+        print("Content-type: application/json")
+        print("Status: 200 OK\n")
+        print(results_json)
+
+
+def test():
+    print("Content-type: text/html")
+    print("Status: 200 OK")
+    print("""
+    <html>
+    <head><title> REST lab </title></head>
+    """)
+    print("<p> %s\n </p>" % path_info.strip("/"))
+    print("</body>")
+    print("</hmtl>")
+
+
+def test_json():
+    x = [1,2,3]
+    x_json = json.dumps(x, indent=2)
+    print("Content-type: application/json")
+    print("Status: 200 OK\n")
+    print(x_json)
+
+
+if path_info.strip("/") == "test":
+    test()
+elif path_info.strip("/") == "json_test":
+    test_json()
+elif path_info.strip("/") == "ff":
+    gets()
+elif pattern.match(path_info) != None:
+    gets()
+elif path_info == "":
+    index()
+elif path_info.strip("/") == "new_player":
+    newPlayer()
+elif path_info.strip("/") == "add_player":
+    addPlayer()
+else:
+    print("Status: 302 Redirect")
+    print("Location: redirect.py\n")
